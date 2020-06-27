8f40a3edec1cc9b58639693b033c5ac46d9d15c6
JoeDGit <joe.dobbins89@gmail.com>
2020-06-27 15:29:40 +0100

    Rspec updates

---
+++ ruby_testing/lib/calculator.rb
@@ -0,0 +1,12 @@
+#lib/calculator.rb
+
+class Calculator
+  def add(a, b, c = 0)
+    a + b + c
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+end
--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -1,9 +1,25 @@
 #spec/calculator_spec.rb
-describe Calculator do 
-  describe '#add' do
-   it 'returns the sum of two numbers' do
-    calculator = Calculator.new
-    expect(calculator.add(5, 2)).to eql(7)
+require './lib/calculator'
+describe Calculator do
+  describe "#add" do
+    it "returns the sum of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
+    end
+  
+    
+    it "returns the sum of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
+    end
+  end
+end
+
+describe Calculator do
+  describe "#multiply" do
+    it "multiplies two numbers together" do
+      calculator = Calculator.new
+      expect(calculator.multiply(6, 5)).to eql(30)
     end
   end
 end
